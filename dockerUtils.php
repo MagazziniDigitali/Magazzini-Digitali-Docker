dd91743a4fdfc213bfe7c8a5b4ff2cd879b20484
svalo <svalo@libersoft.it>
2017-03-23 11:19:33 +0100

    Tabs are now 4 spaces

--- dockerUtils.php
+++ dockerUtils.php
@@ -7,11 +7,11 @@ use Docker\DockerClient;
 use Docker\API\Model\ContainerConfig;
 use Docker\API\Model\HostConfig;
 
-define( "DOCKER_HOST",  "192.168.7.5");
-define( "MAX_CONTAINER","250");
-define( "FIRS_PORT",	"5900");
-define( "FIREFOXURL", "http://md-www.test.bncf.lan/index.php/opac/");
-define( "REMOTE_SOCKET", "tcp://127.0.0.1:2375/v1.24");
+define( "DOCKER_HOST",      "192.168.7.5");
+define( "MAX_CONTAINER",    "250");
+define( "FIRS_PORT",        "5900");
+define( "FIREFOXURL",       "http://md-www.test.bncf.lan/index.php/opac/");
+define( "REMOTE_SOCKET",    "tcp://127.0.0.1:2375/v1.24");
 
 class dockerUtilsException extends Exception {
 
@@ -27,75 +27,86 @@ $docker = new Docker($client);
 
 //TODO:Check if docker is started
 try {
-	$containerManager = $docker->getContainerManager();
+    $containerManager = $docker->getContainerManager();
 } catch (\Exception $e) {
-	print ( $e->getMessage() );
+    print ( $e->getMessage() );
 }
 //Generate a container configuration with a base set of properties
 function genContainerConfig ($ffurl=FIREFOXURL,
-			     $port="5900", $image="local/ffviewer:standard") {
+    $port="5900", $image="local/ffviewer:standard") {
 
-	//Define manager and components to set properties
-	$containerConfig = new ContainerConfig();
-	$containerHostConfig = new HostConfig();
+    //Define manager and components to set properties
+    $containerConfig = new ContainerConfig();
+    $containerHostConfig = new HostConfig();
 
-	//Defines host settings
-	$containerHostConfig->setMemory(360000000);
-	$containerHostConfig->setPortBindings(["5900/tcp" => [["HostPort" => $port]]]);
-	$containerHostConfig->setPublishAllPorts(False);
-	$containerHostConfig->setPrivileged(False);
-	$containerHostConfig->setCapAdd("NET_ADMIN");
-	$containerHostConfig->setCapDrop("MKNOD");
-	$containerHostConfig->setNetworkMode("bridge");
+    //Defines host settings
+    $containerHostConfig->setMemory(360000000);
+    $containerHostConfig->setPortBindings(["5900/tcp" => [["HostPort" => $port]]]);
+    $containerHostConfig->setPublishAllPorts(False);
+    $containerHostConfig->setPrivileged(False);
+    $containerHostConfig->setCapAdd("NET_ADMIN");
+    $containerHostConfig->setCapDrop("MKNOD");
+    $containerHostConfig->setNetworkMode("bridge");
 
-	//Defines general container settings
-	$containerConfig->setAttachStdin(False);
-	$containerConfig->setAttachStdout(False);
-	$containerConfig->setAttachStderr(False);
-	$containerConfig->setTty(False);
-	$containerConfig->setOpenStdin(False);
-	$containerConfig->setStdinOnce(False);
-	$containerConfig->setImage($image);
-	$containerConfig->setNetworkDisabled(False);
-	$containerConfig->setExposedPorts(['5900/tcp' => [''=>'']]);
-	$containerConfig->setEnv(["FFURL=$ffurl"]);
-	$containerConfig->setCmd(
-				["/usr/bin/x11vnc",
-				"-input",
-				"MBK,MBK",
-				"-timeout",
-				"5",
-				"-nopw",
-				"-create",
-				"-gone",
-				"touch /root/left.txt",
-				"-env",
-				"FD_PROG=/usr/bin/viewer.sh"]
-				);
-	$containerConfig->setHostConfig($containerHostConfig);
+    //Defines general container settings
+    $containerConfig->setAttachStdin(False);
+    $containerConfig->setAttachStdout(False);
+    $containerConfig->setAttachStderr(False);
+    $containerConfig->setTty(False);
+    $containerConfig->setOpenStdin(False);
+    $containerConfig->setStdinOnce(False);
+    $containerConfig->setImage($image);
+    $containerConfig->setNetworkDisabled(False);
+    $containerConfig->setExposedPorts(['5900/tcp' => [''=>'']]);
+    $containerConfig->setEnv(["FFURL=$ffurl"]);
+    $containerConfig->setCmd(
+                        ["/usr/bin/x11vnc",
+                        "-input",
+                        "MBK,MBK",
+                        "-timeout",
+                        "5",
+                        "-nopw",
+                        "-create",
+                        "-gone",
+                        "touch /root/left.txt",
+                        "-env",
+                        "FD_PROG=/usr/bin/viewer.sh"]
+                         );
+    $containerConfig->setHostConfig($containerHostConfig);
 
-	return $containerConfig;
+    return $containerConfig;
 }
 
 //Create a container with firefox url and port set
 function createContainer($containerManager, $port, $ffurl=FIREFOXURL, $image){
 
-	//Sets the values for this instance
-	//TODO: cath errors
-	$contConfig = genContainerConfig($ffurl, $port, $image);
+    //Sets the values for this instance
+    //TODO: cath errors
+    $contConfig = genContainerConfig($ffurl, $port, $image);
 
-	$containerCreateResult = $containerManager->create($contConfig);
+    $containerCreateResult = $containerManager->create($contConfig);
 
-	if ( $containerCreateResult->getWarnings() == Null ) {
-		return $containerCreateResult->getId();
-	} else {
-		throw new dockerUtilsException( 'Failed to create container: '.$containerCreateResult);
-	}
+    if ( $containerCreateResult->getWarnings() == Null ) {
+        return $containerCreateResult->getId();
+    } else {
+        throw new dockerUtilsException( 'Failed to create container: '.$containerCreateResult);
+    }
 
 };
 
 //Start the container passed as ID
 function startContainer($containerManager, $id){
+    $containerStartResult = $containerManager->start($id);
+    $isStarted = $containerStartResult->getStatusCode();
+    switch ($isStarted) {
+        case 204:
+            return TRUE;
+        case 304:
+            throw new dockerUtilsException ('Container already started');
+        case 404:
+            throw new dockerUtilsException ('Container does not exist');
+        case 500:
+            throw new dockerUtilsException ('Generic server error');
         default:
             throw new dockerUtilsException ('Invalid response from Docker');
     }
@@ -104,27 +115,27 @@ function startContainer($containerManager, $id){
 //Returns the firs available port starting from 5900
 function portCheck(){
 
-	$host = DOCKER_HOST;
-	$port = FIRS_PORT;
-	$max_container = MAX_CONTAINER;
-	$open = true;
-	$max_port = $port+$max_container;
-	while ( ($port < $max_port) && $open ) {
-		$test = @fsockopen( $host, $port, $timeout = 3 );
-		if ( !$test ){
-			$open = false;
-		}
-		else{
-			fclose( $test );
-			$open = true;
-			$port += 1;
-		}
-	}
-	if ( $open ){
-		throw new dockerUtilsException('No available port left');
-	}
-	else{
-		return $port;
-	}
+    $host = DOCKER_HOST;
+    $port = FIRS_PORT;
+    $max_container = MAX_CONTAINER;
+    $open = true;
+    $max_port = $port+$max_container;
+    while ( ($port < $max_port) && $open ) {
+        $test = @fsockopen( $host, $port, $timeout = 3 );
+        if ( !$test ){
+            $open = false;
+        }
+        else{
+            fclose( $test );
+            $open = true;
+            $port += 1;
+        }
+    }
+    if ( $open ){
+        throw new dockerUtilsException('No available port left');
+    }
+    else{
+        return $port;
+    }
 }
 ?>
