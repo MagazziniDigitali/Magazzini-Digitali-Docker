f90b990a8b38de0e4842ce71ceac8168c5165c49
svalo <svalo@libersoft.it>
2017-03-23 12:56:41 +0100

    Tabs are now 4 spaces

--- index.php
+++ index.php
@@ -7,27 +7,26 @@ require 'ticketInterface.php';
 
 define("TICKET_LEN", 36);
 
-
 class getParameterException extends Exception {
 
 }
 
 // Returns right container image for required type, exception raised if wrong or unset
 function getRequestedContainerType ($type , $imagesAvailable ) {
-	if ( (! isset( $type )) or (! in_array($type, array_keys($imagesAvailable) ) ) ){
-		throw new getParameterException('Requested content type is unsupported or unset');
-	}
-	return $imagesAvailable["$type"];
+    if ( (! isset( $type )) or (! in_array($type, array_keys($imagesAvailable) ) ) ){
+        throw new getParameterException('Requested content type is unsupported or unset');
+    }
+    return $imagesAvailable["$type"];
 }
 // Returns ticket string from client call, exception raised if wrong or unset
 function getRequestedTicket ($ticket) {
-		if (! isset($ticket) ) {
-			throw new getParameterException('No access ticket supplied');
-		} elseif ( TICKET_LEN == mb_strlen($ticket)) {
-			return $ticket;
-		} else {
-			throw new getParameterException('Supplied ticket is in an unknown format');
-		}
+    if (! isset($ticket) ) {
+        throw new getParameterException('No access ticket supplied');
+    } elseif ( TICKET_LEN == mb_strlen($ticket)) {
+        return $ticket;
+    } else {
+        throw new getParameterException('Supplied ticket is in an unknown format');
+    }
 }
 
 
@@ -39,69 +38,69 @@ function getRequestedTicket ($ticket) {
 
 $isUserAuthorized = FALSE;
 try {
-	$ticket = getRequestedTicket ($_GET['idTicket']);
+    $ticket = getRequestedTicket ($_GET['idTicket']);
 } catch(getParameterException $e) {
-	//print ( $e->getMessage() );
-	header('Location: /error.php?err=1');
-	exit();
+    //print ( $e->getMessage() );
+    header('Location: /error.php?err=1');
+    exit();
 }
 
 try {
-	$authenticationUserOutput = checkWsdlTicket($CheckTicketUrl, $CheckTicketUsername, $CheckTicketPassword, $ticket );
+    $authenticationUserOutput = checkWsdlTicket($CheckTicketUrl, $CheckTicketUsername, $CheckTicketPassword, $ticket );
 } catch(WsdlException $e) {
-	//print ( $e->getMessage() );
-	header('Location: /error.php?err=2');
-	exit();
+    //print ( $e->getMessage() );
+    header('Location: /error.php?err=2');
+    exit();
 }
 
 if (!empty($authenticationUserOutput->errorMsg)){
-	print ("Error Msg: ".$authenticationUserOutput->errorMsg->errorType." - ".$authenticationUserOutput->errorMsg->msgError."\n");
-	header('Location: /error.php?err=3');
- 	exit();
+    print ("Error Msg: ".$authenticationUserOutput->errorMsg->errorType." - ".$authenticationUserOutput->errorMsg->msgError."\n");
+    header('Location: /error.php?err=3');
+     exit();
 } else {
-	$isUserAuthorized = TRUE;
-	$url = $authenticationUserOutput->url;
-	try {
-	$image = getRequestedContainerType($authenticationUserOutput->tipo, $imagesAvailable );
-	} catch(getParameterException $e) {
-		//print ( $e->getMessage() );
-		header('Location: /error.php?err=4');
-		exit();
-	}
+    $isUserAuthorized = TRUE;
+    $url = $authenticationUserOutput->url;
+    try {
+    $image = getRequestedContainerType($authenticationUserOutput->tipo, $imagesAvailable );
+    } catch(getParameterException $e) {
+        //print ( $e->getMessage() );
+        header('Location: /error.php?err=4');
+        exit();
+    }
 }
 
 ///////////////////////////////////
 //                               //
-//	Actually creating containers //
+// Actually creating containers  //
 //                               //
 ///////////////////////////////////
 if ( $isUserAuthorized ) {
-	//Checks for firs available port in range
-	try {
-		$port = (string)portCheck();
-	} catch(dockerUtilsException $e) {
-		//print ( 'Risorse terminate, riprova più tardi' );
-		header('Location: /error.php?err=5');
-		exit();
-	}
-	try {
-		$containerId = createContainer($containerManager, $port, $url, $image);
-	} catch(dockerUtilsException $e) {
-		//print ( 'Il documento richiesto non può essere visualizzato,sigh'."\n".'Err: '.$e->getMessage() );
-		header('Location: /error.php?err=6');
-		exit();
-	}
-	try {
-		$isStarted = startContainer($containerManager, $containerId);
-	} catch(dockerUtilsException $e) {
-		//print ('Il documento richiesto non può essere visualizzato, sigh'."\n".'Err: '.$e->getMessage());
-		header('Location: /error.php?err=7');
-		exit();
-	}
-	if ( $isStarted ){
-		$src_url=$_SERVER["SERVER_NAME"];
-		$webpage = "http://$src_url/vnc.html?port=$port";
-		header( 'Location: '.$webpage);
-		}
+    //Checks for firs available port in range
+    try {
+        $port = (string)portCheck();
+    } catch(dockerUtilsException $e) {
+        //print ( 'Risorse terminate, riprova più tardi' );
+        header('Location: /error.php?err=5');
+        exit();
+    }
+    try {
+        $containerId = createContainer($containerManager, $port, $url, $image);
+    } catch(dockerUtilsException $e) {
+        //print ( 'Il documento richiesto non può essere visualizzato,sigh'."\n".'Err: '.$e->getMessage() );
+        header('Location: /error.php?err=6');
+        exit();
+    }
+    try {
+        $isStarted = startContainer($containerManager, $containerId);
+    } catch(dockerUtilsException $e) {
+        //print ('Il documento richiesto non può essere visualizzato, sigh'."\n".'Err: '.$e->getMessage());
+        header('Location: /error.php?err=7');
+        exit();
+    }
+    if ( $isStarted ){
+        $src_url=$_SERVER["SERVER_NAME"];
+        $webpage = "http://$src_url/vnc.html?port=$port";
+        header( 'Location: '.$webpage);
+        }
 }
 ?>
